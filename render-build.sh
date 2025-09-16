ea5b6c25e3d7e36578d9bddb6b1d195dc11aecee
zidilyes <ilyeszid33@gmail.com>
2025-09-16 23:42:34 +0100

    'theFirstDeployEver'

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Mettre à jour PHP à la bonne version
-sudo apt-get update
-sudo apt-get install -y php8.2-cli php8.2-common php8.2-mysql php8.2-xml php8.2-mbstring unzip curl
-
-# Installer Composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
 # Installer les dépendances Symfony
 composer install --no-dev --optimize-autoloader
 
